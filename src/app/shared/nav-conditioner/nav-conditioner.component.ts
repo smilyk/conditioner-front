29f5346c0b086c6571925e10112f6fcc9062c475
natalya khrebtiivsky <smilyk1982@gmail.com>
2020-09-09 15:54:08 +0300

    change nav-panel

--- src/app/shared/nav-conditioner/nav-conditioner.component.ts
+++ src/app/shared/nav-conditioner/nav-conditioner.component.ts
@@ -14,24 +14,16 @@ interface NavLink {
 })
 export class NavConditionerComponent implements OnInit {
   homeLinks: NavLink[] = [
+    {path: Redirect.HOME, label: 'главная страница', icon: 'home'},
+    ];
+  conditionerLinks: NavLink[] = [
     {path: Redirect.ADD_CONDITIONER, label: 'добавить кодиционер', icon: 'books'},
     {path: Redirect.CONDITIONERS_LIST, label: 'список кондиционеров', icon: 'library_books'},
+    ];
+  typeMaintenanceLinks: NavLink[] = [
     {path: 'maint/add', label: 'добавить ТО', icon: 'books'},
     {path: 'maint', label: 'список ТО', icon: 'library_books'},
-    {path: 'instr', label: 'инструкция', icon: 'folder_open'}
-  ];
-  loginLink: NavLink = {path: 'Redirect.LOGIN', label: 'Login', icon: 'person'};
-  logoutLinks: NavLink =    {path: 'Redirect.LOGOUT', label: 'Logout', icon: 'person_outline'};
-  menuItems: NavLink [] = [
-    {path: 'Redirect.BOOK_AUTHORS', label: 'Books of Author', icon: 'books'},
-    {path: 'Redirect.READER_DELAYING', label: 'ReadersDelaying', icon: 'person_add_disabled'},
-    {path: 'Redirect.ACTIVE_READERS', label: 'ActiveReaders', icon: 'sentiment_satisfied_alt'},
-    {path: 'Redirect.AUTHORS_LIST', label: 'ManageAuthors', icon: 'people'},
-    {path: 'Redirect.READERS_LIST', label: 'ManageReaders', icon: 'people_outline'},
-    {path: 'Redirect.ADD_READER', label: 'AddReader', icon: 'person_add'},
-  ];
-
-
+    ];
   constructor() { }
 
   ngOnInit(): void {
